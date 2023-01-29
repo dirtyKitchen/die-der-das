f8d922acccb2e6b80039fa19b10b30b6c54bc38d
dirtyKitchen <mydirtykitchen@outlook.com>
2023-01-29 22:15:10 +0000

    New Image logo

--- index.js
+++ index.js
@@ -1,56 +1,33 @@
-let numbers = [];
-// Initialize an empty array called "numbers"
+var data = [];
+var currentIndex = -1;
 
-for (let i = 1; i <= 5; i++) {
-    numbers.push(i);
-    // Add the integers from 1 to 1000 to the "numbers" array
+// Fetch data from data.json
+fetch('data.json')
+  .then(response => response.json())
+  .then(jsonData => {
+    data = jsonData;
+    getNewData();
+  });
+
+// Get a new data
+function getNewData() {
+  var index = currentIndex;
+  while (index === currentIndex) {
+    index = Math.floor(Math.random() * data.length);
+  }
+  currentIndex = index;
+  document.getElementById('image').src = data[index].image;
+  document.getElementById('title').innerHTML = data[index].title;
+  document.getElementById('description').innerHTML = data[index].description;
 }
 
-let artikleArray = [
-    {
-        path: "image Comes here",
-        name : "Zug (DE) / Train (EN)",
-        artikle : "Der",
-        desc: "Der Zug is zu schnell"
-    },
-    {
-        path: "image Comes here",
-        name : "Katze (DE) / Cat (EN)",
-        artikle : "Die",
-        desc: "Die Katze ist klug"
-    },
-    {
-        path: "image Comes here",
-        name : "Ei (DE) / Egg (EN)",
-        artikle : "Das",
-        desc: "Des Ei ist gesund"
-    },
-    {
-        path: "image Comes here",
-        name : "Maus (DE) / Mouse (EN)",
-        artikle : "Die",
-        desc: "Die Maus ist sehr schnell"
-    },
-    {
-        path: "image Comes here",
-        name : "Buch (DE) / Book (EN)",
-        artikle : "Das",
-        desc: "Das Buch ist nicht interrsant"
-    },   
-]
-let randomNumbers = [];
-// Initialize an empty array called "randomNumbers"
+// Save the current index in local storage
+chrome.storage.local.set({'currentIndex': currentIndex});
 
-while (numbers.length > 0) {
-    let randomIndex = Math.floor(Math.random() * numbers.length);
-    // Generate a random index between 0 and the length of the "numbers" array
-    
-    randomNumbers.push(numbers[randomIndex]);
-    console.log(artikleArray[randomIndex])
-    // Add the element at the random index to the "randomNumbers" array
-    
-    numbers.splice(randomIndex, 1);
-    // Remove the element at the random index from the "numbers" array so that it is not selected again
-}
-// console.log(randomNumbers);
-// Print the "randomNumbers" array to the console
+// Load the last index from local storage
+chrome.storage.local.get(['currentIndex'], function(result) {
+  currentIndex = result.currentIndex;
+});
+
+// Get a new data every day
+setInterval(getNewData, 86400000);
